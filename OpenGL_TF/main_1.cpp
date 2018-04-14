7ca7abb4a45b2fb47a9d75781c5ca123662becef
davidseah <ssc1987@gmail.com>
2018-04-14 11:50:15 +0800

    added uniform

--- OpenGL_TF/main_1.cpp
+++ OpenGL_TF/main_1.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-
-
 #include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+#include <cmath>
 
 #include "Shader.h"
-#include <GLFW\glfw3.h>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -35,6 +35,40 @@ int main()
 	}
 
 	Shader ourShader("../shader.vs", "../shader.frag");
+	//vertices
+	float vertices[] =
+	{
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 3, //first triangle 
+		1, 2, 3 //second traingle
+	};
+
+	unsigned int VAO;
+	unsigned int EBO;
+	unsigned int VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//Check what this does
+	glEnableVertexAttribArray(0);
 
 
 	while (!glfwWindowShouldClose(window))
@@ -44,52 +78,18 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//vertices
-		float vertices[] = 
-		{
-			0.5f, 0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			-0.5f, -0.5f, 0.0f,
-			-0.5f, 0.5f, 0.0f 
-		};
-
-		unsigned int indices[] =
-		{
-			0, 1, 3, //first triangle 
-			1, 2, 3 //second traingle
-		};
-
-
-		
-		
-		unsigned int VAO;
-		unsigned int EBO;
-		unsigned int VBO;
-
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &EBO);
-		glBindVertexArray(VAO);
-
-
-
-		
-
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-		(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		//Check what this does
-		glEnableVertexAttribArray(0);
-
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue)/2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(ourShader.Program, "ourColor");
 		ourShader.Use();
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, greenValue, 1.0f);
+
+		
 		glBindVertexArray(VAO);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//Check what is this
 		glBindVertexArray(0);
 
 
